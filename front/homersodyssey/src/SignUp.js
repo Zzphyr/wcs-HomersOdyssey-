a3f9f230ab8e824a772048b31fdc469a79d8a55d
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-12-16 20:00:36 +0000

    add quest020 code, fix package vulnerability

--- front/homersodyssey/src/SignUp.js
+++ front/homersodyssey/src/SignUp.js
@@ -4,20 +4,49 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      passwordBis: "monPassw0rd",
+      name: "James",
+      lastname: "Bond"
     }
   }
 
   updateEmailField = (event) => {
     this.setState({email: event.target.value});
   }  
+  updatePasswordField = (event) => {
+    this.setState({password: event.target.value});
+  }  
+  updatePasswordBisField = (event) => {
+    this.setState({passwordBis: event.target.value});
+  }  
+  updateNameField = (event) => {
+    this.setState({name: event.target.value});
+  }  
+  updateLastnameField = (event) => {
+    this.setState({lastname: event.target.value});
+  }  
   
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("A name was submitted: ", this.state)
+  }
+  
+
   render() {
     const { email } = this.state;
     return(
       <> 
-        <h1>{email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField} />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="email" name="email" onChange={this.updateEmailField} />
+          <input type="password" name="password" onChange={this.updatePasswordField} />
+          <input type="password" name="passwordBis" onChange={this.updatePasswordBisField} />
+          <input type="name" name="name" onChange={this.updateNameField} />
+          <input type="lastname" name="lastname" onChange={this.updateLastnameField} />
+          <input type="submit" value="Submit"/>
+        </form>
       </>
     );
   }
